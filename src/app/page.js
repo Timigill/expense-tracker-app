4048de39cb47bf5106413391439c03965f591954
Timigill <juttt4680@gmail.com>
2024-12-01 22:55:53 +0500

    f1

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import styles from "./page.module.css";
+// import styles from "@app/page.module.css";
 
 import React, { useState } from 'react';
 import TransactionForm from '@/components/transactionform/TransactionForm';
